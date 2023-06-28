dc55f9d569a624accd2736c7df2495c97c1dc77f
Takaki Takeuchi <takaki@takeu.ch>
2023-06-28 18:43:05 +0900

    feat: get current temperature and humidity via SwitchBot API

--- api/app/controllers/smart_home/meter_controller.rb
+++ api/app/controllers/smart_home/meter_controller.rb
@@ -1,11 +1,23 @@
 # frozen_string_literal: true
 
+require 'switchbot'
+
+SWITCHBOT_METER_DEVICE_ID = ENV.fetch('SWITCHBOT_METER_DEVICE_ID', '')
+
 module SmartHome
   # Meter Controller
   class MeterController < ApplicationController
     def index
-      temperature = 26.3
-      humidity = 59
+      switchbot_client = Switchbot::Client.new(
+        ENV.fetch('SWITCHBOT_TOKEN', ''),
+        ENV.fetch('SWITCHBOT_SECRET', '')
+      )
+
+      meter_status = switchbot_client.device(SWITCHBOT_METER_DEVICE_ID).status[:body]
+
+      # extract temperature and humidity
+      temperature = meter_status[:temperature]
+      humidity = meter_status[:humidity]
 
       render json: { data: { temperature:, humidity: } }
     end
